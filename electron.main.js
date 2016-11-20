0e8136ed42fa7b8c6902e0157b11742263489b21
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-11-20 19:21:23 +0700

    Initialize game

--- electron.main.js
+++ electron.main.js
@@ -15,9 +15,9 @@ function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  // and load the index.html of the app.
+  // and load the build/index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, '/build/index.html'),
     protocol: 'file:',
     slashes: true
   }))
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// Webpack configuration
+// http://webpack.github.io/docs/configuration.html
+var webpack = require('webpack');
+var path = require('path');
+
+var SRC_DIR = path.resolve(__dirname, 'src');
+var BUILD_DIR = path.resolve(__dirname, 'build');
+
+var config = {
+    context: SRC_DIR,
+    entry: SRC_DIR + '/webpack.entry.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'webpack.output.js'
+    },
+    module: {
+        loaders : [
+            {test : /\.jsx?/, include : SRC_DIR,loader : 'babel'}
+        ]
+    }
+};
+
+module.exports = config;
